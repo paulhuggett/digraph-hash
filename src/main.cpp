dd3fdaace4638f38a921d1eb047452f8170d062c
Paul Bowen-Huggett <paulhuggett@mac.com>
2020-01-15 13:50:47 +0100

    Split the test code out using google test.

--- src/main.cpp
+++ src/main.cpp
@@ -12,211 +12,30 @@ namespace {
     using vertex_digest_pair = std::tuple<vertex const *, hash::digest>;
     using graph_digests = std::vector<vertex_digest_pair>;
 
-    // Iterator is an STL iterator type which will produce an instance of type vertex_digest_pair.
-    template <typename Iterator>
-    void dump (std::ostream & os, Iterator first, Iterator last) {
-        char const * separator = "  ";
-        std::for_each (first, last, [&] (auto const & t) {
-            os << separator << std::get<vertex const *> (t)->name () << ':' << std::hex
-               << std::get<hash::digest> (t);
-            separator = ", ";
-        });
-        os << '\n';
-    }
+} // end anonymous namespace
 
-    /// \tparam Iterator An iterator type which will produce an instance of type vertex.
-    template <typename Iterator>
-    std::tuple<memoized_hashes, graph_digests> hash_vertices (Iterator first, Iterator last) {
-        graph_digests digests;
-        memoized_hashes table;
-
-        std::for_each (first, last, [&digests, &table] (vertex const & v) {
-            digests.emplace_back (&v, vertex_hash (&v, &table));
-        });
-
-        // Sort the result by vertex name. This means that the iteration order won't have any
-        // effect on the output.
-        using value_type = graph_digests::value_type;
-        std::sort (std::begin (digests), std::end (digests),
-                   [] (value_type const & x, value_type const & y) {
-                       return std::get<0> (x)->name () < std::get<0> (y)->name ();
-                   });
-        dump (std::cout, std::begin (digests), std::end (digests));
-        return {std::move (table), std::move (digests)};
-    }
-
-    /// digraph G {
-    ///    c -> a;
-    ///    c -> b;
-    /// }
-    void simple_test () {
-        std::list<vertex> graph;
-        auto & va = graph.emplace_back ("a");
-        auto & vb = graph.emplace_back ("b");
-        auto & vc = graph.emplace_back ("c").adjacent ({&va, &vb}); // c -> a; c -> b;
-
-        auto const h1 = hash_vertices (std::begin (graph), std::end (graph));
-
-        auto const & mh1 = std::get<memoized_hashes> (h1);
-        assert (mh1.size () == 3U);
-        assert (mh1.count (&va) == 1U);
-        assert (mh1.count (&vb) == 1U);
-        assert (mh1.count (&vc) == 1U);
-
-        auto const h2 = hash_vertices (std::rbegin (graph), std::rend (graph));
-
-        auto const & mh2 = std::get<memoized_hashes> (h2);
-        assert (mh2.size () == 3U);
-        assert (mh2.count (&va) == 1U);
-        assert (mh2.count (&vb) == 1U);
-        assert (mh2.count (&vc) == 1U);
-
-        auto const & vdp1 = std::get<graph_digests> (h1);
-        auto const & vdp2 = std::get<graph_digests> (h2);
-        assert (
-            std::equal (std::begin (vdp1), std::end (vdp1), std::begin (vdp2), std::end (vdp2)));
-    }
-
-    /// digraph G {
-    ///    a -> b;
-    ///    b -> a;
-    /// }
-    void tiny_loop () {
-        std::list<vertex> graph;
-        auto & va = graph.emplace_back ("a");
-        auto & vb = graph.emplace_back ("b").adjacent (&va); // b -> a;
-        va.adjacent (&vb); // a -> b;
-
-        auto const h1 = hash_vertices (std::begin (graph), std::end (graph));
-        assert (std::get<memoized_hashes> (h1).size () == 0U);
-
-        auto const h2 = hash_vertices (std::rbegin (graph), std::rend (graph));
-        assert (std::get<memoized_hashes> (h2).size () == 0U);
-
-        auto const & vdp1 = std::get<graph_digests> (h1);
-        auto const & vdp2 = std::get<graph_digests> (h2);
-        assert (
-            std::equal (std::begin (vdp1), std::end (vdp1), std::begin (vdp2), std::end (vdp2)));
-    }
-
-    /// digraph G {
-    ///    a -> b;
-    ///    b -> a;
-    ///    c -> a;
-    ///    d -> c;
-    /// }
-    ///
-    /// The result hashes shoud be:
-    ///
-    /// | Name | |
-    /// | "a" | Va/Vb/R0<br>
-    ///         Vertex "a" -> vertex "b" -> loop back to the first (zeroth) vertex. The slash in
-    ///         this notation can be thought of a directed edge between the two adjacent vertices.
-    /// | "b" | Vb/Va/R0 |
-    /// | "c" | Vc/Va/Vb/R1 |
-    /// | "d" | Vd/Vc/Va/Vb/R2 |
-    void test_looped () {
-        std::list<vertex> graph;
-        auto & va = graph.emplace_back ("a");
-        auto & vb = graph.emplace_back ("b").adjacent (&va); // b -> a;
-        va.adjacent (&vb); // a -> b;
-        auto & vc = graph.emplace_back ("c").adjacent (&va); // c -> a;
-        graph.emplace_back ("d").adjacent (&vc);             // d -> c;
-
-        auto const forward_result = hash_vertices (std::begin (graph), std::end (graph));
-        assert (std::get<memoized_hashes> (forward_result).size () == 0);
-
-        auto const reverse_result = hash_vertices (std::rbegin (graph), std::rend (graph));
-        assert (std::get<memoized_hashes> (reverse_result).size () == 0);
-
-        auto const & vdp1 = std::get<graph_digests> (forward_result);
-        auto const & vdp2 = std::get<graph_digests> (reverse_result);
-        assert (
-            std::equal (std::begin (vdp1), std::end (vdp1), std::begin (vdp2), std::end (vdp2)));
-    }
-
-
-    /// digraph G {
-    ///     c -> b -> a -> c;
-    ///     f -> e -> d -> f;
-    ///     g -> c;
-    ///     g -> f;
-    /// }
-    void two_loops () {
-        std::list<vertex> graph;
-        auto & va = graph.emplace_back ("a");
-        auto & vb = graph.emplace_back ("b").adjacent (&va); // b -> a
-        auto & vc = graph.emplace_back ("c").adjacent (&vb); // c -> b
-        va.adjacent (&vc);                                   // a -> c
-        auto & vd = graph.emplace_back ("d");
-        auto & ve = graph.emplace_back ("e").adjacent (&vd); // e -> d
-        auto & vf = graph.emplace_back ("f").adjacent (&ve); // f -> e
-        vd.adjacent (&vf);                                   // d -> f
-        graph.emplace_back ("g").adjacent ({&vc, &vf});      // g -> c; g -> f;
-
-        auto const h1 = hash_vertices (std::begin (graph), std::end (graph));
-        assert (std::get<memoized_hashes> (h1).size () == 0U);
-
-        auto const h2 = hash_vertices (std::rbegin (graph), std::rend (graph));
-        assert (std::get<memoized_hashes> (h2).size () == 0U);
-
-        auto const & vdp1 = std::get<graph_digests> (h1);
-        auto const & vdp2 = std::get<graph_digests> (h2);
-        assert (
-            std::equal (std::begin (vdp1), std::end (vdp1), std::begin (vdp2), std::end (vdp2)));
-    }
-
+int main () {
     /// digraph G {
     ///     a -> b;
     ///     b -> a;
     ///     c -> d;
     /// }
-    void two_islands () {
-        std::list<vertex> graph;
-        auto & va = graph.emplace_back ("a");
-        auto & vb = graph.emplace_back ("b").adjacent (&va);
-        va.adjacent (&vb);
-        auto & vc = graph.emplace_back ("c");
-        auto & vd = graph.emplace_back ("d").adjacent (&vc);
+    std::list<vertex> graph;
+    auto & va = graph.emplace_back ("a");
+    auto & vb = graph.emplace_back ("b").adjacent (&va);
+    va.adjacent (&vb);
+    auto & vc = graph.emplace_back ("c");
+    graph.emplace_back ("d").adjacent (&vc);
 
-        auto const h1 = hash_vertices (std::begin (graph), std::end (graph));
-        auto const & mh1 = std::get<memoized_hashes> (h1);
-        assert (mh1.size () == 2U);
-        assert (mh1.count (&vc) == 1U);
-        assert (mh1.count (&vd) == 1U);
+    graph_digests digests;
+    memoized_hashes table;
+    std::transform (std::begin (graph), std::end (graph),
+                    std::inserter (digests, std::end (digests)), [&table] (vertex const & v) {
+                        return vertex_digest_pair{&v, vertex_hash (&v, &table)};
+                    });
 
-        auto const h2 = hash_vertices (std::rbegin (graph), std::rend (graph));
-        auto const & mh2 = std::get<memoized_hashes> (h2);
-        assert (mh2.size () == 2U);
-        assert (mh2.count (&vc) == 1U);
-        assert (mh2.count (&vd) == 1U);
-
-        auto const & vdp1 = std::get<graph_digests> (h1);
-        auto const & vdp2 = std::get<graph_digests> (h2);
-        assert (
-            std::equal (std::begin (vdp1), std::end (vdp1), std::begin (vdp2), std::end (vdp2)));
-    }
-
-} // end anonymous namespace
-
-int main () {
-    std::cout << "Simple test:\n";
-    simple_test ();
-    std::cout << "Bytes hashed so far: " << std::dec << hash::total () << '\n';
-
-    std::cout << "Tiny loop test:\n";
-    tiny_loop ();
-    std::cout << "Bytes hashed so far: " << std::dec << hash::total () << '\n';
-
-    std::cout << "Test with loop:\n";
-    test_looped ();
-    std::cout << "Total bytes hashed: " << std::dec << hash::total () << '\n';
-
-    std::cout << "Two loops:\n";
-    two_loops ();
-    std::cout << "Bytes hashed: " << std::dec << hash::total () << '\n';
-
-    std::cout << "Two islands:\n";
-    two_islands ();
-    std::cout << "Bytes hashed: " << std::dec << hash::total () << '\n';
+    std::for_each (std::begin (digests), std::end (digests), [] (auto const & vdp) {
+        std::cout << std::get<vertex const *> (vdp)->name () << ':' << std::hex
+                  << std::get<hash::digest> (vdp) << '\n';
+    });
 }
---
+++ unittest/test.cpp
@@ -0,0 +1,205 @@
+#include <list>
+
+#include <algorithm>
+#include <iterator>
+#include <functional>
+#include <set>
+
+#include <gmock/gmock.h>
+
+#include "hash.hpp"
+#include "memhash.hpp"
+#include "vertex.hpp"
+
+using testing::Eq;
+using testing::UnorderedElementsAre;
+
+namespace std {
+
+    template <>
+    struct hash<vertex const *> {
+        size_t operator() (vertex const * const v) const noexcept {
+            return hash<std::string>{}(v->name ());
+        }
+    };
+
+} // namespace std
+
+namespace {
+
+    using vertex_digest_pair = std::tuple<vertex const *, hash::digest>;
+    using graph_digests = std::vector<vertex_digest_pair>;
+
+    /// \tparam Iterator An iterator type which will produce an instance of type vertex.
+    template <typename Iterator>
+    std::tuple<memoized_hashes, graph_digests> hash_vertices (Iterator first, Iterator last) {
+        graph_digests digests;
+        memoized_hashes table;
+
+        std::for_each (first, last, [&digests, &table] (vertex const & v) {
+            digests.emplace_back (&v, vertex_hash (&v, &table));
+        });
+
+        // Sort the result by vertex name. This means that the iteration order won't have any
+        // effect on the output.
+        using value_type = graph_digests::value_type;
+        std::sort (std::begin (digests), std::end (digests),
+                   [] (value_type const & x, value_type const & y) {
+                       return std::get<0> (x)->name () < std::get<0> (y)->name ();
+                   });
+        return {std::move (table), std::move (digests)};
+    }
+
+    template <typename Iterator>
+    auto keys (Iterator first, Iterator last) {
+        using value_type = typename std::iterator_traits<Iterator>::value_type;
+        std::set<typename value_type::first_type> result;
+        std::transform (first, last, std::inserter (result, result.end ()),
+                        [] (value_type const & v) { return v.first; });
+        return result;
+    }
+
+} // end anonymous namespace
+
+// Test behavior with a graph of the form:
+//
+//     digraph G {
+//        c -> a;
+//        c -> b;
+//     }
+TEST (DigraphHash, Simple) {
+    std::list<vertex> graph;
+    auto & va = graph.emplace_back ("a");
+    auto & vb = graph.emplace_back ("b");
+    auto const & vc = graph.emplace_back ("c").adjacent ({&va, &vb}); // c -> a; c -> b;
+
+    auto const expected_cache = UnorderedElementsAre (&va, &vb, &vc);
+
+    // Forward
+    auto const h1 = hash_vertices (std::begin (graph), std::end (graph));
+    auto const & mh1 = std::get<memoized_hashes> (h1);
+    EXPECT_THAT (keys (std::begin (mh1), std::end (mh1)), expected_cache);
+
+    // Reverse
+    auto const h2 = hash_vertices (std::rbegin (graph), std::rend (graph));
+    auto const & mh2 = std::get<memoized_hashes> (h2);
+    EXPECT_THAT (keys (std::begin (mh2), std::end (mh2)), expected_cache);
+
+    // Check that digests are the same for the two traversal orders.
+    EXPECT_THAT (std::get<graph_digests> (h1), Eq (std::get<graph_digests> (h2)))
+        << "Output should not be affected by traversal order";
+}
+
+/// digraph G {
+///    a -> b -> a;
+/// }
+TEST (DigraphHash, TinyLoop) {
+    std::list<vertex> graph;
+    auto & va = graph.emplace_back ("a");
+    auto & vb = graph.emplace_back ("b").adjacent (&va); // b -> a;
+    va.adjacent (&vb);                                   // a -> b;
+
+    // Forward
+    auto const h1 = hash_vertices (std::begin (graph), std::end (graph));
+    EXPECT_EQ (std::get<memoized_hashes> (h1).size (), 0U)
+        << "Expected nothing to be memoized for this graph";
+
+    // Reverse
+    auto const h2 = hash_vertices (std::rbegin (graph), std::rend (graph));
+    EXPECT_EQ (std::get<memoized_hashes> (h2).size (), 0U)
+        << "Expected nothing to be memoized for this graph";
+
+    // Check that digests are the same for the two traversal orders.
+    EXPECT_THAT (std::get<graph_digests> (h1), Eq (std::get<graph_digests> (h2)))
+        << "Output should not be affected by traversal order";
+}
+
+/// digraph G {
+///    d -> c -> a -> b -> a;
+/// }
+TEST (DigraphHash, Loop2) {
+    std::list<vertex> graph;
+    auto & va = graph.emplace_back ("a");
+    auto & vb = graph.emplace_back ("b").adjacent (&va); // b -> a;
+    va.adjacent (&vb);                                   // a -> b;
+    auto & vc = graph.emplace_back ("c").adjacent (&va); // c -> a;
+    graph.emplace_back ("d").adjacent (&vc);             // d -> c;
+
+    // Forward
+    auto const forward_result = hash_vertices (std::begin (graph), std::end (graph));
+    EXPECT_EQ (std::get<memoized_hashes> (forward_result).size (), 0U)
+        << "Expected nothing to be memoized for this graph";
+
+    // Reverse
+    auto const reverse_result = hash_vertices (std::rbegin (graph), std::rend (graph));
+    EXPECT_EQ (std::get<memoized_hashes> (reverse_result).size (), 0U)
+        << "Expected nothing to be memoized for this graph";
+
+    // Check that digests are the same for the two traversal orders.
+    EXPECT_THAT (std::get<graph_digests> (forward_result),
+                 Eq (std::get<graph_digests> (reverse_result)))
+        << "Output should not be affected by traversal order";
+}
+
+/// digraph G {
+///     g -> c -> b -> a -> c;
+///     g -> f -> e -> d -> f;
+/// }
+TEST (DigraphHash, TwoLoops) {
+    std::list<vertex> graph;
+    auto & va = graph.emplace_back ("a");
+    auto & vb = graph.emplace_back ("b").adjacent (&va); // b -> a
+    auto & vc = graph.emplace_back ("c").adjacent (&vb); // c -> b
+    va.adjacent (&vc);                                   // a -> c
+    auto & vd = graph.emplace_back ("d");
+    auto & ve = graph.emplace_back ("e").adjacent (&vd); // e -> d
+    auto & vf = graph.emplace_back ("f").adjacent (&ve); // f -> e
+    vd.adjacent (&vf);                                   // d -> f
+    graph.emplace_back ("g").adjacent ({&vc, &vf});      // g -> c; g -> f;
+
+    // Forward
+    auto const h1 = hash_vertices (std::begin (graph), std::end (graph));
+    EXPECT_EQ (std::get<memoized_hashes> (h1).size (), 0U)
+        << "Expected nothing to be memoized for this graph";
+
+    // Reverse
+    auto const h2 = hash_vertices (std::rbegin (graph), std::rend (graph));
+    EXPECT_EQ (std::get<memoized_hashes> (h2).size (), 0U)
+        << "Expected nothing to be memoized for this graph";
+
+    // Check that digests are the same for the two traversal orders.
+    EXPECT_THAT (std::get<graph_digests> (h1), Eq (std::get<graph_digests> (h2)))
+        << "Output should not be affected by traversal order";
+}
+
+/// digraph G {
+///     a -> b -> a;
+///     c -> d;
+/// }
+TEST (DigraphHash, TwoIslands) {
+    std::list<vertex> graph;
+    auto & va = graph.emplace_back ("a");
+    auto & vb = graph.emplace_back ("b").adjacent (&va);
+    va.adjacent (&vb);
+    auto & vc = graph.emplace_back ("c");
+    auto & vd = graph.emplace_back ("d").adjacent (&vc);
+
+    auto const expected_cache = UnorderedElementsAre (&vc, &vd);
+
+    // Forward
+    auto const h1 = hash_vertices (std::begin (graph), std::end (graph));
+    {
+        auto const & mh1 = std::get<memoized_hashes> (h1);
+        EXPECT_THAT (keys (std::begin (mh1), std::end (mh1)), expected_cache);
+    }
+
+    // Reverse
+    auto const h2 = hash_vertices (std::rbegin (graph), std::rend (graph));
+    {
+        auto const & mh2 = std::get<memoized_hashes> (h2);
+        EXPECT_THAT (keys (std::begin (mh2), std::end (mh2)), expected_cache);
+    }
+    // Check that digests are the same for the two traversal orders.
+    EXPECT_THAT (std::get<graph_digests> (h1), Eq (std::get<graph_digests> (h2)))
+        << "Output should not be affected by traversal order";
+}
